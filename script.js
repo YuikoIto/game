79b60d317d96d75c69c5c519c9cc03239ae93de1
YuikoIto <toffywing@gmail.com>
2020-07-25 03:28:03 +0900

    make range for damage

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+const damageRange = 0.2;
+
 const playerData = {
     name: "プレイヤー",
-    hp: 100
+    hp: 100,
+    attack: 5,
+    deffence: 2
 }
 
 const enemyData = {
     name: "敵",
-    hp: 500
+    hp: 500,
+    attack: 4,
+    deffence: 1
+}
+
+function damageCalculation(attack, deffence) {
+    const maxDamage = attack * (1+damageRange);
+    const minDamage = attack * (1-damageRange);
+    const attackDamage = Math.floor(Math.random() * (maxDamage - minDamage) + minDamage);
+    
+    const damage = attackDamage - deffence;
+
+    if (damage < 1) {
+        return 0;
+    } else {
+        return damage;
+    }
 }
 
 function insertText(id, text) {
@@ -18,3 +38,30 @@ insertText("maxPlayerHp", playerData["hp"]);
 insertText("enemyName", enemyData["name"]);
 insertText("currentEnemyHp", enemyData["hp"]);
 insertText("maxEnemyHp", enemyData["hp"]);
+
+document.getElementById("attack").addEventListener("click", function(){
+    let endGame = false;
+
+    const playerDamage = damageCalculation(playerData["attack"], enemyData["deffence"]);
+    const enemyDamage = damageCalculation(enemyData["attack"], enemyData["deffence"]);
+
+    enemyData["hp"] -= enemyDamage;
+    playerData["hp"] -= playerDamage;
+    insertText("currentEnemyHp", enemyData["hp"]);
+    insertText("currentPlayerHp", playerData["hp"]);
+
+    if (enemyData["hp"] <= 0){
+        alert('勝利');
+        endGame = true;
+        enemyData["hp"] = 0;
+        insertText("currentEnemyHp", enemyData["hp"]);
+    } else if (playerData["hp"] <= 0) {
+        alert('負け');
+        endGame = true;
+        playerData["hp"] = 0;
+        insertText("currentPlayerHp", playerData["hp"]);
+    } 
+    if (endGame) {
+        this.classList.add("deactive");
+    }
+});
